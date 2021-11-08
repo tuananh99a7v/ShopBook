db27068c17b3680ebc37918028918b99d1b5a2a9
TuanAnh <tuananh99a7v@gmail.com>
2021-11-08 18:45:24 +0700

    Add project files.

---
+++ BookStore.Model/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Model.Models
+{
+	[Table("Author")]
+	public class Author
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int AuthorId { get; set; }
+		public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Email { get; set; }
+		public string Address { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ BookStore.Model/Models/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Models
+{
+	
+	[Table("Publisher")]
+	public class Publisher
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int PublisherId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
