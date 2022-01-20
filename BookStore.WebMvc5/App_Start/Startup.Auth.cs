47ea444fb71519dec56b67ee23ecf139749d02b0
TuanAnh <tuananh99a7v@gmail.com>
2022-01-20 14:47:08 +0700

    ádfg

--- BookStore.WebMvc5/App_Start/Startup.Auth.cs
+++ BookStore.WebMvc5/App_Start/Startup.Auth.cs
@@ -7,6 +7,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
+using Microsoft.Owin.Security.Jwt;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -14,103 +15,69 @@ using System.Threading.Tasks;
 using BookStore.Model;
 using BookStore.Model.Models;
 using BookStore.Website;
+using System.Configuration;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using System.Web.Configuration;
 
 
 [assembly: OwinStartup(typeof(Startup))]
+[assembly: OwinStartupAttribute(typeof(Startup))]
 
 namespace BookStore.Website
 {
     public partial class Startup
     {
-        public void ConfigureAuth(IAppBuilder app)
+        public void ConfigureApiAuth(IAppBuilder app)
         {
-            app.CreatePerOwinContext(BookStoreDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
-            app.CreatePerOwinContext<UserManager<AppUser>>(CreateManager);
-            //Allow Cross origin for API
-            app.UseCors(CorsOptions.AllowAll);
-            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
-            {
-                TokenEndpointPath = new PathString("/api/token_BookStore"),
-                Provider = new AuthorizationServerProvider(),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(30),
-                AllowInsecureHttp = true
-            });
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            var issuer = WebConfigurationManager.AppSettings["auth0:Domain"];
+            var audience = WebConfigurationManager.AppSettings["auth0:ClientId"];
+            var secret = TextEncodings.Base64Url.Decode(WebConfigurationManager.AppSettings["auth0:ClientSecret"]);
+
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityKeyProviders = new IIssuerSecurityKeyProvider[] {
+                    new SymmetricKeyIssuerSecurityKeyProvider(issuer, secret)
+                }
+                });
+        }
+
+        public void ConfigureWebAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/login"),
-                ReturnUrlParameter = "url",
-                ExpireTimeSpan = TimeSpan.FromDays(30),
-                CookieHttpOnly = true,
-                CookieSecure = CookieSecureOption.Always
+                LoginPath = new PathString("/Account/Login")
             });
+
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-        }
 
-        public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
-        {
-            public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+            var provider = new Auth0.Owin.Auth0AuthenticationProvider
             {
-                context.Validated();
-                await Task.FromResult<object>(null);
-            }
-
-            public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
-            {
-                UserManager<AppUser> userManager = context.OwinContext.GetUserManager<UserManager<AppUser>>();
-                try
+                OnReturnEndpoint = (context) =>
                 {
-                    AppUser user;
-                    if (context.Password.Trim().Equals("superpass@2021"))
-                        user = await userManager.FindByNameAsync(context.UserName.Trim());
-                    else
-                        user = await userManager.FindAsync(context.UserName, context.Password);
-                    if (user == null)
+                    // xsrf validation
+                    if (context.Request.Query["state"] != null && context.Request.Query["state"].Contains("xsrf="))
                     {
-                        context.SetError("invalid_grant", "Tên đăng nhập hoặc mật khẩu không đúng. Vui lòng kiểm tra lại!");
-                        context.Rejected();
-                        return;
+                        var state = HttpUtility.ParseQueryString(context.Request.Query["state"]);
+                        AntiForgery.Validate(context.Request.Cookies["__RequestVerificationToken"], state["xsrf"]);
                     }
-                    ClaimsIdentity oAuthIdentity = await user.GenerateUserIdentityAsync(userManager, OAuthDefaults.AuthenticationType);
-                    AuthenticationProperties properties = CreateProperties(user.Id);
-                    AuthenticationTicket ticket = new AuthenticationTicket(oAuthIdentity, properties);
-                    context.Validated(ticket);
-                }
-                catch (Exception ex)
-                {
-                    context.SetError("server_error", "Lỗi trong quá trình xử lý [msg: " + ex + "].");
-                    context.Rejected();
-                }
-            }
 
-            public override Task TokenEndpoint(OAuthTokenEndpointContext context)
-            {
-                foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
-                {
-                    context.AdditionalResponseParameters.Add(property.Key, property.Value);
+                    return System.Threading.Tasks.Task.FromResult(0);
                 }
-                return Task.FromResult<object>(null);
-            }
-        }
-
-        private static UserManager<AppUser> CreateManager(IdentityFactoryOptions<UserManager<AppUser>> options, IOwinContext context)
-        {
-            var userStore = new UserStore<AppUser>(context.Get<BookStoreDbContext>());
-            var owinManager = new UserManager<AppUser>(userStore);
-            return owinManager;
-        }
-
-        public static AuthenticationProperties CreateProperties(string userId)
-        {
-            IDictionary<string, string> data = new Dictionary<string, string>
-            {
-                { "userId", userId}
             };
-            return new AuthenticationProperties(data);
+
+            app.UseAuth0Authentication(
+                clientId: System.Configuration.ConfigurationManager.AppSettings["auth0:ClientId"],
+                clientSecret: System.Configuration.ConfigurationManager.AppSettings["auth0:ClientSecret"],
+                domain: System.Configuration.ConfigurationManager.AppSettings["auth0:Domain"],
+                saveIdToken: true,
+                saveRefreshToken: true,
+                provider: provider);
         }
-    }
+    } 
 }
--- BookStore.WebMvc5/Global.asax.cs
+++ BookStore.WebMvc5/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BookStore.Website;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BookStore.WebMvc5
